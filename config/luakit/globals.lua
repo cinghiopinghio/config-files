44e8fe4b39c05f791826c4f51d89a2c226ade7b3
Cinghio Pinghio <cinghio@linuxmail.org>
2014-11-28 15:20:12 +0100

    added some configs

---
+++ config/luakit/globals.lua
@@ -0,0 +1,16 @@
+
+
+search_engines = {
+  sp           = "http://startpage.com/do/metasearch.pl?query=%s",
+  aur          = "https://aur.archlinux.org/packages.php?O=0&K=%s&do_Search=Go",
+  aw           = "https://wiki.archlinux.org/index.php/Special:Search?fulltext=Search&search=%s",
+  googleseceng = "https://www.google.com/search?name=f&hl=en&q=%s",
+  schoolar     = "http://scholar.google.it/scholar?q=%s",
+}
+
+search_engines.default = search_engines.sp
+
+globals = {
+  homepage = "https://startpage.com/do/mypage.pl?prf=98676c1095beb6ccc8f1bbbbb02f82d2",
+}
+
