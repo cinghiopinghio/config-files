e8a899e118b37a8e4fd8293ff5cb306a221bcfd7
Cinghio Pinghio <cinghio@linuxmail.org>
2014-12-01 12:27:54 +0100

    quite there

--- dotinstall.py
+++ dotinstall.py
@@ -13,6 +13,8 @@ NOINSTALL = [\
              'dotinstall.py'\
             ]
 
+VERB = True
+
 def main(inifile='install.ini'):
     """main
 
@@ -21,30 +23,65 @@ def main(inifile='install.ini'):
     """
 
     conf = co.ConfigObj('install.ini')
-    follow = { c.split('/')[0]:c for c in conf['fixed'] }
-    print (follow)
-    print (conf)
+    if VERB:
+        print (title("Configuration:"))
+        for k,v in conf.items():
+            print ('{0:>15s} -> {1:<15s}'.format(k,str(v)))
 
-
-    for f in os.listdir('.'):
-        if f[0] == '.' or f in NOINSTALL or f in conf['noinstall']:
-            pass
-        elif f in follow.keys():
-            path = follow[f]
-            if path in conf['rename'].keys():
-                newname = conf['rename'][path]
-                linkit(f,fixedpath=path,rename=newname)
+    paths = get_paths(conf)
+    if VERB:
+        print (title('Installs:'))
+        for p in paths:
+            print ("{0[0]:_<45s} {0[1]:15s}".format(p))
+            if os.path.exists(p[1]):
+                if os.path.islink(p[1]):
+                    print ('already installed')
+                else:
+                    print ('installing')
             else:
-                linkit(f,fixedpath=path)
-        else:
-            linkit(f)
-
+                print('new')
 
     for hook,command in conf['hooks'].items():
-        print (hook,command)
+        if VERB:
+            print (title('Executing post-hooks:'))
+            print (hook,'...',command)
             
 
-def linkit(localfile, fixedpath=None, rename=None):
+def title(titlestring):
+    return '\n{0:-<75s}\n'.format(titlestring)
+
+def get_paths(conf,folder='.'):
+    """Get paths where to put links.
+
+    :conf: configuration object
+    :returns: list of 2-tuples (local file,path of link)
+
+    """
+
+    fix = {}
+    for f in conf['fixed']:
+        basef = f.split('/')[0]
+        if basef in fix.keys():
+            fix[basef].append(f)
+        else:
+            fix[basef] = [f]
+
+    paths = []
+    for loc_path in os.listdir('.'):
+        if loc_path[0] == '.' or loc_path in NOINSTALL\
+           or loc_path in conf['noinstall']:
+            pass
+        elif loc_path in fix.keys():
+            path = fix[loc_path]
+            p = get_path (loc_path, fixedpath=path, rename=conf['rename'],\
+                         folder=folder)
+            paths.extend(p)
+        else:
+            p = get_path(loc_path, folder=folder)
+            paths.append(p)
+    return paths
+
+def get_path(localfile, fixedpath=None, rename=None, folder='.'):
     """TODO: Docstring for linkit.
 
     :localfile: TODO
@@ -53,19 +90,32 @@ def linkit(localfile, fixedpath=None, rename=None):
 
     """
 
+
+    base = os.path.abspath(folder)
+    home = os.path.expanduser('~')
+    
     if fixedpath==None:
         if rename is None:
-            homefile = '~/.'+ localfile
+            homefile = home+'/.'+ localfile
         else:
-            homefile = '~/.'+ rename
-        #print (localfile, homefile)
+            homefile = home+'/.'+ rename
+        return (base+'/'+localfile, homefile)
     else:
-        files = os.listdir(fixedpath)
-        for f in files:
-            if rename is None:
-                linkit(fixedpath+'/'+f,None)
-            else:
-                linkit(fixedpath+'/'+f,None,rename+'/'+f)
+        files = []
+        for fp in fixedpath:
+            _files = [ fp+'/'+fl for fl in os.listdir(fp) ]
+            for _fp in fixedpath:
+                if _fp in files:
+                    files.remove(_fp)
+            files.extend(_files)
+        
+        for rn in rename.keys():
+            # rename where needed
+            files = [ (f,f.replace(rn,rename[rn])) for f in files ]
+            # add basenames folders
+            files = [ (base+'/'+f,home+'/.'+rn) for f,rn in files ]
+
+    return files
 
 
 if __name__ == '__main__':
