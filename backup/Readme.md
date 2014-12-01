4b5a23e121935780f2a43c83d26f5759325f2ad1
Cinghio Pinghio <cinghio@linuxmail.org>
2014-12-01 15:27:17 +0100

    pretty much working, lack of command line interpreter

---
+++ backup/Readme.md
@@ -0,0 +1,3 @@
+# this is a backup directory
+backed up files will be copied here. You should care aboud versioning them
+or not.
--- dotinstall.py
+++ dotinstall.py
@@ -3,6 +3,7 @@
 
 import sys,os
 import configobj as co
+import time
 
 NOINSTALL = [\
              'README.md',\
@@ -13,8 +14,8 @@ NOINSTALL = [\
              'dotinstall.py'\
             ]
 
-VERB = True
-INTERACTIVE = False
+VERB = False
+INTERACTIVE = True
 FAKEHOME = True
 
 def main(inifile='install.ini',folder='.'):
@@ -31,11 +32,9 @@ def main(inifile='install.ini',folder='.'):
             print ('{0:>15s} -> {1:<15s}'.format(k,str(v)))
 
     paths = get_paths(conf,folder)
-    if VERB:
-        print (title('Installs'))
+    if VERB: print (title('Installs'))
     for p in paths:
-        if VERB:
-            print ("---- {0[1]:15s}".format(p))
+        if VERB:  print ("Installing", p[1])
         install(p[0],p[1])
             
     for hook,command in conf['hooks'].items():
@@ -43,13 +42,15 @@ def main(inifile='install.ini',folder='.'):
             print (title('Executing post-hooks'))
             print (hook,'...',command)
 
-def install(local_path, real_path, yesall=True):
+def install(local_path, real_path, yesall=False):
     """create a link from real_path to local_path:
 
     :local_path: path
     :real_path: path
 
     """
+
+    prefix='   ->  '
     
     if not os.path.exists(local_path):
         print (local_path,': no such file')
@@ -59,13 +60,39 @@ def install(local_path, real_path, yesall=True):
         if os.path.islink(real_path) and\
            local_path == os.path.realpath(real_path):
 
-            print (local_path, 'is already installed on ', real_path)
+            if VERB: print (prefix, 'Already installed!')
 
         else:
-            print ('installing  -  starting diff command')
-            os.system('meld ' + real_path + ' ' + local_path)
+            print (prefix,'Already prensent in filesystem:')
+            print (prefix,prefix,'1. Overwrite')
+            print (prefix,prefix,'2. Det diff with diff-tool')
+            print (prefix,prefix,'3. Backup and install')
+            print (prefix,prefix,'4. Skip this')
+
+            choice = input('Choice: [1234]')
+            if choice == '1': 
+                #overwrite
+                removepath(real_path)
+                link(local_path, real_path)
+            elif choice == '2':
+                os.system('meld ' + real_path + ' ' + local_path)
+                removepath(real_path)
+                link(local_path, real_path)
+            elif choice == '3':
+                dirname,basename = os.path.split(local_path)
+                bup_dir = '{:s}/backup/{:s}-{:s}'.format(dirname,\
+                                                         basename,\
+                                        time.strftime('-%Y-%m-%d_%H-%M-%S')
+                                                        )
+                #os.rename(real_path, bup_dir) #<- do not work
+                os.system( 'mv '+real_path+' '+bup_dir)
+                link(local_path, real_path)
+            elif choice == '4':
+                pass
+            else:
+                pass
     else:
-        print('new')
+        print(prefix, 'Installing new instance...')
         installit = False
         if INTERACTIVE:
             inter = input('Install {0:s}? [y/N]'.format(local_path)).lower()
@@ -75,19 +102,34 @@ def install(local_path, real_path, yesall=True):
             installit = True
 
         if installit:
-            print ('OK')
+            link(local_path, real_path)
 
-            # check if folder tree exists otherwise create it
-            dirname = os.path.dirname(real_path)
-            print (dirname)
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
+def removepath(path):
+    """remove path
 
-            # link it
-            print(local_path, real_path)
-            os.system('ln -s ' + local_path + ' ' + real_path)
+    :path: path
 
+    """
+    if os.path.isdir(path):
+        os.removedirs(path)
+    else:
+        os.remove(path)
+    
+def link(local_path, real_path):
+    """Actually link it
 
+    :local_path: path
+    :real_path: path
+
+    """
+    # check if folder tree exists otherwise create it
+    dirname = os.path.dirname(real_path)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname,exist_ok=True)
+
+    # link it
+    os.symlink(local_path, real_path)
+    
 def title(titlestring):
     return '\n{0:-^75s}\n'.format(titlestring)
 
@@ -160,7 +202,6 @@ def get_path(localfile, fixedpath=None, rename=None, folder='.'):
             files = [ (base+'/'+f,home+'/.'+rn) for f,rn in files ]
 
     return files
-
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
