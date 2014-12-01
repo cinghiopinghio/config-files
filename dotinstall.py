4a39f092da6024ca6669d0a0a944f599b523e01a
Cinghio Pinghio <cinghio@linuxmail.org>
2014-12-01 14:15:45 +0100

    running

--- dotinstall.py
+++ dotinstall.py
@@ -14,8 +14,10 @@ NOINSTALL = [\
             ]
 
 VERB = True
+INTERACTIVE = False
+FAKEHOME = True
 
-def main(inifile='install.ini'):
+def main(inifile='install.ini',folder='.'):
     """main
 
     :inifile: config file path.
@@ -24,31 +26,70 @@ def main(inifile='install.ini'):
 
     conf = co.ConfigObj('install.ini')
     if VERB:
-        print (title("Configuration:"))
+        print (title("Configuration"))
         for k,v in conf.items():
             print ('{0:>15s} -> {1:<15s}'.format(k,str(v)))
 
-    paths = get_paths(conf)
+    paths = get_paths(conf,folder)
     if VERB:
-        print (title('Installs:'))
-        for p in paths:
-            print ("{0[0]:_<45s} {0[1]:15s}".format(p))
-            if os.path.exists(p[1]):
-                if os.path.islink(p[1]):
-                    print ('already installed')
-                else:
-                    print ('installing')
-            else:
-                print('new')
-
+        print (title('Installs'))
+    for p in paths:
+        if VERB:
+            print ("---- {0[1]:15s}".format(p))
+        install(p[0],p[1])
+            
     for hook,command in conf['hooks'].items():
         if VERB:
-            print (title('Executing post-hooks:'))
+            print (title('Executing post-hooks'))
             print (hook,'...',command)
-            
+
+def install(local_path, real_path, yesall=True):
+    """create a link from real_path to local_path:
+
+    :local_path: path
+    :real_path: path
+
+    """
+    
+    if not os.path.exists(local_path):
+        print (local_path,': no such file')
+        exit(11)
+
+    if os.path.exists(real_path):
+        if os.path.islink(real_path) and\
+           local_path == os.path.realpath(real_path):
+
+            print (local_path, 'is already installed on ', real_path)
+
+        else:
+            print ('installing  -  starting diff command')
+            os.system('meld ' + real_path + ' ' + local_path)
+    else:
+        print('new')
+        installit = False
+        if INTERACTIVE:
+            inter = input('Install {0:s}? [y/N]'.format(local_path)).lower()
+            if inter == 'y':
+                installit = True
+        elif yesall:
+            installit = True
+
+        if installit:
+            print ('OK')
+
+            # check if folder tree exists otherwise create it
+            dirname = os.path.dirname(real_path)
+            print (dirname)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
+            # link it
+            print(local_path, real_path)
+            os.system('ln -s ' + local_path + ' ' + real_path)
+
 
 def title(titlestring):
-    return '\n{0:-<75s}\n'.format(titlestring)
+    return '\n{0:-^75s}\n'.format(titlestring)
 
 def get_paths(conf,folder='.'):
     """Get paths where to put links.
@@ -67,7 +108,7 @@ def get_paths(conf,folder='.'):
             fix[basef] = [f]
 
     paths = []
-    for loc_path in os.listdir('.'):
+    for loc_path in os.listdir(folder):
         if loc_path[0] == '.' or loc_path in NOINSTALL\
            or loc_path in conf['noinstall']:
             pass
@@ -92,7 +133,10 @@ def get_path(localfile, fixedpath=None, rename=None, folder='.'):
 
 
     base = os.path.abspath(folder)
-    home = os.path.expanduser('~')
+    if FAKEHOME:
+        home = ('/tmp/home')
+    else:
+        home = os.path.expanduser('~')
     
     if fixedpath==None:
         if rename is None:
