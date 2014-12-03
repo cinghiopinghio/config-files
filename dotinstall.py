8212353624a6b455b5533f7ebbe338e6499caafc
Cinghio Pinghio <cinghio@linuxmail.org>
2014-12-03 12:05:10 +0100

    argparse and colorama

--- dotinstall.py
+++ dotinstall.py
@@ -4,6 +4,8 @@
 import sys,os,shutil
 import configobj as co
 import time
+from colorama import Fore
+import argparse
 
 NOINSTALL = [\
              'README.md',\
@@ -19,32 +21,92 @@ VERB = True
 INTERACTIVE = True
 FAKEHOME = False
 
-def main(inifile='install.ini',folder='.'):
+class DotFile():
+    """Each dotfile"""
+
+    def __init__(self, local, home='~', config='.'):
+        """init
+
+        :local: TODO
+        :path: TODO
+
+        """
+        self.home = os.path.expanduser(home)
+        self.config = os.path.abspath(os.path.expanduser(config))
+
+        self.local = local
+        self.path = '.' + local
+
+    def get_path(self):
+        """returns path where to link
+        :returns: path
+
+        """
+        return self.home + '/' + self.path
+
+    def get_local(self):
+        """returns file path
+        :returns: path
+
+        """
+        return self.config + '/' + self.local
+
+    def renamePath(self, rename):
+        """rename self._path according to rename
+
+        :rename: dict
+        :returns: None
+
+        """
+
+        for old, new in rename.iteritems():
+            if old in self.local:
+                self.path = self.path.replace(old, new)
+
+    def __eq__(self, other):
+        """__eq__
+
+        :other: Other class
+        :returns: Self == Other
+
+        """
+        return self.local == other.local
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+
+    def __str__(self):
+        """__str__
+        :returns: TODO
+
+        """
+        return self._locale
+        
+def main(args):
     """main
 
-    :inifile: config file path.
+    :args: command line args.
 
     """
 
-    conf = co.ConfigObj('install.ini')
-    if VERB:
-        print (title("Configuration"))
-        for k,v in conf.items():
-            print ('{0:>15s} -> {1:<15s}'.format(k,str(v)))
 
-    paths = get_paths(conf,folder)
-    if VERB: print (title('Installs'))
-    for p in paths:
-        if VERB:  print ("Installing", p[1])
-        install(p[0],p[1])
+    conf = co.ConfigObj(args.configfile)
+    log("Configuration",'T')
+    for k,v in conf.items():
+        log('{0:>15s} -> {1:<15s}'.format(k,str(v)))
+
+    paths = add_paths(conf,args.dir)
+    log('Installs','T')
+    install(paths, dry=args.dry)
             
     for hook,command in conf['hooks'].items():
-        if VERB:
-            print (title('Executing post-hooks'))
-            print (hook,'...',command)
+        log ('Executing post-hooks','T') 
+        log (hook+' ... '+command)
         os.system(command)
 
-def install(local_path, real_path, yesall=False):
+def install(dotfiles, dry=False, yesall=False):
     """create a link from real_path to local_path:
 
     :local_path: path
@@ -52,60 +114,106 @@ def install(local_path, real_path, yesall=False):
 
     """
 
-    prefix='   ->  '
-    
-    if not os.path.exists(local_path):
-        print (local_path,': no such file')
-        exit(11)
+    for df in dotfiles:
+        local = df.get_local()
+        path = df.get_path()
+        if not os.path.exists(local):
+            log (df.local+': no such file','E')
+            exit(11)
 
-    if os.path.exists(real_path):
-        if os.path.islink(real_path) and\
-           local_path == os.path.realpath(real_path):
+        if pathexists(path):
+            if os.path.islink(path) and\
+               local == os.path.realpath(path):
 
-            if VERB: print (prefix, 'Already installed!')
+                log('Already installed!',indent=1)
 
-        else:
-            print (prefix,'Already prensent in filesystem:')
-            print (prefix,prefix,'1. Overwrite')
-            print (prefix,prefix,'2. Det diff with diff-tool')
-            print (prefix,prefix,'3. Backup and install')
-            print (prefix,prefix,'4. Skip this')
+            elif not os.path.exists(os.path.realpath(path)):
 
-            choice = input('Choice: [1234]')
-            if choice == '1': 
-                #overwrite
-                removepath(real_path)
-                link(local_path, real_path)
-            elif choice == '2':
-                os.system('meld ' + real_path + ' ' + local_path)
-                removepath(real_path)
-                link(local_path, real_path)
-            elif choice == '3':
-                dirname,basename = os.path.split(local_path)
-                bup_dir = '{:s}/backup/{:s}-{:s}'.format(dirname,\
-                                                         basename,\
-                                        time.strftime('-%Y-%m-%d_%H-%M-%S')
-                                                        )
-                #os.rename(real_path, bup_dir) #<- do not work
-                #os.system( 'mv '+real_path+' '+bup_dir)
-                shutil.move(real_path, bup_dir)
-                link(local_path, real_path)
-            elif choice == '4':
-                pass
+                log('Path '+path+' is a broken link.',level='W',\
+                       force=True)
+                choice = input('Remove it? [Y/n]').lower()
+                if choice != 'n' and not dry:
+                    os.unlink(path)
+                    link(local, path)
+
             else:
-                pass
-    else:
-        print(prefix, 'Installing new instance...')
-        installit = False
-        if INTERACTIVE:
-            inter = input('Install {0:s}? [y/N]'.format(local_path)).lower()
-            if inter == 'y':
-                installit = True
-        elif yesall:
-            installit = True
+                log('Already prensent in filesystem:',level='W',indent=1,\
+                    force=True)
+                log('1. Overwrite',indent=2,force=True)
+                log('2. Get diff with diff-tool',indent=2,force=True)
+                log('3. Backup and install',indent=2,force=True)
+                log('4. Skip this',indent=2,force=True)
 
-        if installit:
-            link(local_path, real_path)
+                choice = input('Choice: [1234]')
+                if choice == '1' and not dry: 
+                    #overwrite
+                    removepath(path)
+                    link(local, path)
+                elif choice == '2' and not dry:
+                    os.system('meld ' + path + ' ' + local)
+                    removepath(path)
+                    link(local, path)
+                elif choice == '3' and not dry:
+                    dirname,basename = os.path.split(local)
+                    bup = '{:s}/backup/{:s}-{:s}'.format(dirname,\
+                                                             basename,\
+                                            time.strftime('-%Y-%m-%d_%H-%M-%S')
+                                                            )
+                    #os.rename(real_path, bup_dir) #<- do not work
+                    #os.system( 'mv '+real_path+' '+bup_dir)
+                    shutil.move(path, bup)
+                    link(local, path)
+                elif choice == '4' and not dry:
+                    pass
+                else:
+                    pass
+        else:
+            log('Installing new instance...', indent=1)
+            installit = False
+            if INTERACTIVE:
+                inter = input('Install {0:s}? [y/N]'.format(df.local)).lower()
+                if inter == 'y':
+                    installit = True
+            elif yesall:
+                installit = True
+
+            if installit and not dry:
+                link(local, path)
+
+def log(string, level='N', enable=True, indent=0, force=False):
+    """print colored output
+
+    :string: string to print
+    :level: W N E
+    :enable: if colors are enabled
+
+    """
+    ind=''
+    for i in range(indent): ind += '    '
+    if force or VERB:
+        if enable:
+            if level == 'N':
+                out = string
+            elif level == 'W':
+                out = Fore.YELLOW + 'Warning: ' + string + Fore.RESET
+            elif level == 'E':
+                out = Fore.RED + 'Error: ' + string + Fore.RESET
+            elif level == 'T':
+                out = Fore.GREEN + '\n{0:-^75s}\n'.format(string) +\
+                      Fore.RESET
+
+            print(ind+out)
+        else:
+            print(ind+string)
+
+def pathexists(path):
+    """local definition that consider broken links
+
+    :path: TODO
+    :returns: TODO
+
+    """
+    return os.path.islink(path) or os.path.isfile(path) or os.path.isdir(path)
 
 def removepath(path):
     """remove path
@@ -134,10 +242,7 @@ def link(local_path, real_path):
     # link it
     os.symlink(local_path, real_path)
     
-def title(titlestring):
-    return '\n{0:-^75s}\n'.format(titlestring)
-
-def get_paths(conf,folder='.'):
+def add_paths(conf,folder='.'):
     """Get paths where to put links.
 
     :conf: configuration object
@@ -145,72 +250,70 @@ def get_paths(conf,folder='.'):
 
     """
 
-    fix = {}
+    skipfolds = set([])
     for f in conf['fixed']:
-        basef = f.split('/')[0]
-        if basef in fix.keys():
-            fix[basef].append(f)
-        else:
-            fix[basef] = [f]
+        fold = f.split('/')
+        for n in range(len(fold)):
+            p = '/'.join(fold[:n+1])
+            skipfolds.add(p)
+
+    localpaths = []
+    for l in os.listdir(folder):
+        if l not in skipfolds and l not in NOINSTALL\
+           and l not in conf['noinstall'] and l[0] != '.':
+            localpaths.append(l)
+    for f in conf['fixed']:
+        for l in os.listdir(folder+'/'+f):
+            fname = f + '/' + l
+            if fname not in skipfolds:
+                localpaths.append(fname)
 
     paths = []
-    for loc_path in os.listdir(folder):
-        if loc_path[0] == '.' or loc_path in NOINSTALL\
-           or loc_path in conf['noinstall']:
-            pass
-        elif loc_path in fix.keys():
-            path = fix[loc_path]
-            p = get_path (loc_path, fixedpath=path, rename=conf['rename'],\
-                         folder=folder)
-            paths.extend(p)
-        else:
-            p = get_path(loc_path, folder=folder)
-            paths.append(p)
+    for l in localpaths:
+        dotfile = DotFile(l,config=folder)
+        dotfile.renamePath(conf['rename'])
+        paths.append(dotfile)
     return paths
 
-def get_path(localfile, fixedpath=None, rename=None, folder='.'):
-    """TODO: Docstring for linkit.
-
-    :localfile: TODO
-    :fixedpath: TODO
-    :returns: TODO
-
-    """
-
-
-    base = os.path.abspath(folder)
-    if FAKEHOME:
-        home = ('/tmp/home')
-    else:
-        home = os.path.expanduser('~')
-    
-    if fixedpath==None:
-        if rename is None:
-            homefile = home+'/.'+ localfile
-        else:
-            homefile = home+'/.'+ rename
-        return (base+'/'+localfile, homefile)
-    else:
-        files = []
-        for fp in fixedpath:
-            _files = [ fp+'/'+fl for fl in os.listdir(fp) ]
-            for _fp in fixedpath:
-                if _fp in files:
-                    files.remove(_fp)
-            files.extend(_files)
-        
-        for rn in rename.keys():
-            # rename where needed
-            files = [ (f,f.replace(rn,rename[rn])) for f in files ]
-            # add basenames folders
-            files = [ (base+'/'+f,home+'/.'+rn) for f,rn in files ]
-
-    return files
-
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(prog='dotinstall',\
+                                     description='Install dotfiles')
+    parser.add_argument('-c','--color', action='store_true',
+                       help='Colored output')
+    parser.add_argument('-i','--interactive', action='store_true',
+                       help='Ask before installing')
+    parser.add_argument('-v','--verbose', action='store_true',
+                       help='Verbose')
+    parser.add_argument('-a','--all', action='store_true',
+                       help='same as -civ')
+    parser.add_argument('-n','--dry', action='store_true',
+                       help='Perform a dry install (simulation)')
+    parser.add_argument('-t','--test', action='store_true',
+                       help='Perform tests')
+    parser.add_argument('-f','--configfile', \
+                        default = 'install.ini',\
+                        help = 'Configuration file')
+    parser.add_argument('-d','--dir', \
+                        default = '.',\
+                        help = 'Source file directory')
+
+    args = parser.parse_args()
+    if args.all:
+        VERB = True
+        INTERACTIVE = True
+        COLORED = True
+    else:
+        VERB = args.verbose
+        INTERACTIVE = args.interactive
+        COLORED = args.color
+
+    parser.print_help()
+    main(args)
+    exit()
     if len(sys.argv) == 1:
         main()
     elif len(sys.argv) == 2:
         main(sys.argv[1])
     else:
-        print ('Usage: dotinstall.py [ini-file]')
+        log ('Usage: dotinstall.py [ini-file]',level='E',force=True)
