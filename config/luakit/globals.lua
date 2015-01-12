0355d7f813175fc48373deef8b06b0480bac9931
Cinghio Pinghio <cinghio@linuxmail.org>
2015-01-12 16:06:35 +0100

    added wikipedia

--- config/luakit/globals.lua
+++ config/luakit/globals.lua
@@ -1,11 +1,12 @@
 
 
 search_engines = {
-  sp           = "http://startpage.com/do/metasearch.pl?query=%s",
-  aur          = "https://aur.archlinux.org/packages.php?O=0&K=%s&do_Search=Go",
-  aw           = "https://wiki.archlinux.org/index.php/Special:Search?fulltext=Search&search=%s",
-  googleseceng = "https://www.google.com/search?name=f&hl=en&q=%s",
-  schoolar     = "http://scholar.google.it/scholar?q=%s",
+  sp  = "http://startpage.com/do/metasearch.pl?query=%s",
+  aur = "https://aur.archlinux.org/packages.php?O=0&K=%s&do_Search=Go",
+  aw  = "https://wiki.archlinux.org/index.php/Special:Search?fulltext=Search&search=%s",
+  goo = "https://www.google.com/search?name=f&hl=en&q=%s",
+  sch = "http://scholar.google.it/scholar?q=%s",
+  w   = "https://en.wikipedia.org/wiki/%s",
 }
 
 search_engines.default = search_engines.sp
