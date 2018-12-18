0e9aecb4e3ad99519aa43c27ad65aafb0840749f
Cinghio Pinghio <cinghio@linuxmail.com>
2018-12-18 14:42:27 +0100

    clock on the bottom left

--- config-wm/.i3/lock.sh
+++ config-wm/.i3/lock.sh
@@ -17,41 +17,60 @@ convert ${HOME}/.local/share/background.jpg\
   -extent ${newsize}\
   ${TMPF}
 
-i3lock                    \
---insidevercolor=$V       \
---ringvercolor=$V         \
-                          \
---insidewrongcolor=$W     \
---ringwrongcolor=$W       \
-                          \
---insidecolor=$B          \
---ringcolor=$D            \
---linecolor=$N            \
---separatorcolor=$D       \
-                          \
---verifcolor=$T           \
---wrongcolor=$T           \
---timecolor=$T            \
---datecolor=$T            \
---layoutcolor=$T          \
---keyhlcolor=$T           \
---bshlcolor=$W            \
-                          \
---screen 0                \
---blur 5                  \
---clock                   \
---indicator               \
-                          \
---timestr="%H:%M"         \
---time-font="Fira Sans"   \
---datestr="%A, %m %Y"     \
---date-font="Fira Sans"   \
---layout-font="Fira Sans" \
---verif-font="Fira Sans"  \
---wrong-font="Fira Sans"  \
---veriftext="CHECKING..." \
---wrongtext="WRONG!"      \
---ring-width=3            \
--i ${TMPF}
+# i3lock                    \
+# --insidevercolor=$V       \
+# --ringvercolor=$V         \
+#                           \
+# --insidewrongcolor=$W     \
+# --ringwrongcolor=$W       \
+#                           \
+# --insidecolor=$B          \
+# --ringcolor=$D            \
+# --linecolor=$N            \
+# --separatorcolor=$D       \
+#                           \
+# --verifcolor=$T           \
+# --wrongcolor=$T           \
+# --timecolor=$T            \
+# --datecolor=$T            \
+# --layoutcolor=$T          \
+# --keyhlcolor=$T           \
+# --bshlcolor=$W            \
+#                           \
+# --screen 0                \
+# --blur 5                  \
+# --clock                   \
+# --indicator               \
+#                           \
+# --timestr="%H:%M"         \
+# --time-font="Fira Sans"   \
+# --datestr="%A, %m %Y"     \
+# --date-font="Fira Sans"   \
+# --layout-font="Fira Sans" \
+# --verif-font="Fira Sans"  \
+# --wrong-font="Fira Sans"  \
+# --veriftext="CHECKING..." \
+# --wrongtext="WRONG!"      \
+# --ring-width=3            \
+# --image ${TMPF}
+
+	letterEnteredColor=d23c3dff
+	letterRemovedColor=d23c3dff
+	passwordCorrect=00000000
+	passwordIncorrect=d23c3dff
+	background=00000000
+	foreground=ffffffff
+	i3lock \
+		-t -i "$1" \
+		--timepos='x+110:h-70' \
+		--datepos='x+43:h-45' \
+		--clock --date-align 1 \
+		--insidecolor=$background --ringcolor=$foreground --line-uses-inside \
+		--keyhlcolor=$letterEnteredColor --bshlcolor=$letterRemovedColor --separatorcolor=$background \
+		--insidevercolor=$passwordCorrect --insidewrongcolor=$passwordIncorrect \
+		--ringvercolor=$foreground --ringwrongcolor=$foreground --indpos='x+280:h-70' \
+		--radius=20 --ring-width=4 --veriftext='' --wrongtext='' \
+		--verifcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
+		--noinputtext='' --force-clock --image ${TMPF}
 
 rm ${TMPF}
