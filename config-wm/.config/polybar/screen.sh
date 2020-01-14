ad111a78327b80ac9aa91c81e5ddd37eb3619539
Cinghio Pinghio <cinghio@linuxmail.com>
2020-01-14 22:56:56 +0100

    update

--- config-wm/.config/polybar/screen.sh
+++ config-wm/.config/polybar/screen.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+source ~/.config/polybar/colors-gruvbox.sh
+
 SE_STATUS="$(safeeyes --status)"
+TEXT="%{T3}    %{T-}"
 
 if [[ "${1}" == "-t" ]]; then
   case "${SE_STATUS}" in
     Disabled*)
       safeeyes --enable &
       xautolock -enable &
-      killall -SIGUSR2 dunst & # resume
-      echo SCREEN
+      killall -SIGUSR2 dunst 2>>/dev/null & # resume
+      echo ${TEXT}
       ;;
     *)
       safeeyes --disable &
       xautolock -disable &
-      killall -SIGUSR1 dunst & # pause
-      echo "%{B#A00} SCREEN %{B-}"
+      killall -SIGUSR1 dunst 2>>/dev/null & # pause
+      echo "%{B#A00}${TEXT}%{B-}"
       ;;
   esac
 else
   case "${SE_STATUS}" in
     Disabled*)
-      echo "%{B#A00} SCREEN %{B-}"
+      echo "%{B${RED}}${TEXT}%{B-}"
       ;;
     *)
-      echo SCREEN
+      echo ${TEXT}
       ;;
   esac
 fi
